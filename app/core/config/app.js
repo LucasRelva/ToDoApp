36f6595e64f20b35dfcf308ab1770e4c75818613
Lucas <lucas-1880@outlook.com>
2021-06-19 16:55:44 -0300

    Changing path + installing axios :heavy_plus_sign:

--- app/core/config/app.js
+++ app/core/config/app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const cors = require('cors')
-const routes = require('../guards/routes')
+const routes = require('../routes/routes')
+const port = 8000
 
 require('./dbconnection')
 
 const app = express()
 const server = require('http').createServer(app)
 
+// app.use(express.static("public"))
+// app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+
 app.use(routes)
 
-server.listen(8000, () => {
-    console.log('Server running at 8000')
+server.listen(port, () => {
+    console.log('Server running at ' + port)
 })
